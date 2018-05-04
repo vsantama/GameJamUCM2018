c7bd95a56bd946f2f1c5625b63547afe740f8d27
pablodll <pdaurell@ucm.es>
2018-05-04 19:13:37 +0200

    Escenas de pruebas

--- GameJam/Assets/Scripts/PlayerController.cs
+++ GameJam/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float velocity = 20f;
+    public KeyCode[] teclas = new KeyCode[4];
 	
 	// Update is called once per frame
 	void Update () {
@@ -11,19 +12,19 @@ public class PlayerController : MonoBehaviour {
 
     void Move(float vel)
     {
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(teclas[0]))
         {
             transform.Translate(-vel * Time.deltaTime, 0, 0, Space.World);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(teclas[1]))
         {
             transform.Translate(vel * Time.deltaTime, 0, 0, Space.World);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(teclas[2]))
         {
             transform.Translate(0, vel * Time.deltaTime, 0, Space.World);
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(teclas[3]))
         {
             transform.Translate(0, -vel * Time.deltaTime, 0, Space.World);
         }
