bcbf5f54d91b9e9800daa7223c85ab972b11291b
GUILLERMO OVEJERO SANCHEZ <guilleov@ucm.es>
2018-05-04 19:59:26 +0200

    JuntarJugadores
    Hay una escena de prueba para verlo

---
+++ GameJam/Assets/Scripts/JuntarPlayers.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuntarPlayers : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject playerSoloPrefab;
+
+	// Use this for initialization
+	void Start () {
+		//Asignar automaticamente el GO
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("COLLISION");
+        //PASAR DATOS DE PLAYER 1 A PLAYER 2
+        playerSoloPrefab.transform.position = player1.transform.position;
+        Instantiate(playerSoloPrefab);
+        Destroy(player1);
+        Destroy(player2);
+    }
+}
