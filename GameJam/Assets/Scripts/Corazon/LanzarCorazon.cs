2ea785efce481d5f1b56c3bc0fed54f21eabdf2d
Pablo <pablo.daurell@gmail.com>
2018-05-05 22:25:58 +0200

    Trafico de organos

--- GameJam/Assets/Scripts/Corazon/LanzarCorazon.cs
+++ GameJam/Assets/Scripts/Corazon/LanzarCorazon.cs
@@ -13,6 +13,8 @@ public class LanzarCorazon : MonoBehaviour
     public GameObject corazon;
     public KeyCode tecla;
 
+    Vector3 pos;
+
     // Use this for initialization
     void Start()
     {
@@ -30,26 +32,30 @@ public class LanzarCorazon : MonoBehaviour
     {
         if (Input.GetKeyDown(tecla) && Manager.tieneCorazon)
         {
-            corazon.transform.position = transform.position;
             switch(Controller.ultimaTecla)
             {
                 case KeyCode.W:
                 case KeyCode.UpArrow:
                     corazon.GetComponent<MoverCorazon>().direccion = Vector3.up;
+                    pos = new Vector3(0, 1, 0);
                     break;
                 case KeyCode.S:
                 case KeyCode.DownArrow:
                     corazon.GetComponent<MoverCorazon>().direccion = Vector3.down;
+                    pos = new Vector3(0, -1, 0);
                     break;
                 case KeyCode.D:
                 case KeyCode.RightArrow:
                     corazon.GetComponent<MoverCorazon>().direccion = Vector3.right;
+                    pos = new Vector3(1, 0, 0);
                     break;
                 case KeyCode.A:
                 case KeyCode.LeftArrow:
                     corazon.GetComponent<MoverCorazon>().direccion = Vector3.left;
+                    pos = new Vector3(-1, 0, 0);
                     break;
             }
+            corazon.transform.position = transform.position + pos;
             Instantiate(corazon);
             Manager.tieneCorazon = false;
         }
--- GameJam/Assets/Scripts/Corazon/MoverCorazon.cs
+++ GameJam/Assets/Scripts/Corazon/MoverCorazon.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class MoverCorazon : MonoBehaviour {
 
     public Vector3 direccion = new Vector3(0, 0, 0);
-	// Use this for initialization
+    public float vel = 6f;
+    public Rigidbody2D rb;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        transform.Translate(direccion * Time.deltaTime);
+        //transform.Translate(direccion * vel * Time.deltaTime);
+        rb.velocity = direccion * vel * Time.deltaTime * 50f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            collision.gameObject.GetComponent<PlayerManager>().tieneCorazon = true;
+        }
     }
 }
