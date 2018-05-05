1d6a90f965ff374065af227029e86fbd304e6ee5
L <leonorcu@ucm.es>
2018-05-05 03:57:18 +0200

    Botones un click

---
+++ GameJam/Assets/Scripts/HorizontalMovent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovent : MonoBehaviour {
+
+    public float vel = 10;
+    bool activated = false;
+    Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (activated)
+        {
+            pos += Vector3.right;
+            transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * vel);
+        }
+	}
+
+    void Activate()
+    {
+        activated = true;
+    }
+}
