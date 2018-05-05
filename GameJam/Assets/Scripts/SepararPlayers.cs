2685004ea549991cb8ca18aaee137b84e69b8e05
GUILLERMO OVEJERO SANCHEZ <guilleov@ucm.es>
2018-05-05 02:36:03 +0200

    Revert "Revert"
    This reverts commit 6222370a0e30704f7d24ee26d365ac77c3b61613.

--- GameJam/Assets/Scripts/SepararPlayers.cs
+++ GameJam/Assets/Scripts/SepararPlayers.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,14 +7,11 @@ public class SepararPlayers : MonoBehaviour {
     public GameObject player1;
     public GameObject player2;
     public GameObject playerSolo;
-
+    public bool shiftIzq;
+    public bool shiftPressed = false;
     Vector3 pos;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
+
 	// Update is called once per frame
 	void Update () {
         Separar();
@@ -22,8 +19,19 @@ public class SepararPlayers : MonoBehaviour {
 
     void Separar()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+       
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
+            shiftIzq = true;
+            shiftPressed = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.RightShift)) {
+            shiftPressed = true;
+        }
+
+        if (shiftPressed)
+        {
+
             pos = new Vector3(playerSolo.transform.position.x - 0.8f, playerSolo.transform.position.y, playerSolo.transform.position.z);
             player1.transform.position = pos;
 
