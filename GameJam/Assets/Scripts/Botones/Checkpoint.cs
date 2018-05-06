a39b2aa17365c1e393861e875ed667490854c00f
GUILLERMO OVEJERO SANCHEZ <guilleov@ucm.es>
2018-05-06 04:35:26 +0200

    ME CAGO EN DECKARD Nivel 5 No acabado
    Los muros no se mueven

--- GameJam/Assets/Scripts/Botones/Checkpoint.cs
+++ GameJam/Assets/Scripts/Botones/Checkpoint.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Checkpoint : ObjetosActivables {
 
+    public bool player1;
     public Vector3 posicionCheckpoint;
-	// Use this for initialization
-	void Start () {
-        posicionCheckpoint = gameObject.transform.position;
-	}
-	
+    // Use this for initialization
+    private void Update()
+    {
+        if(activated && !player1)
+        {
+            int nextLevel = SliderManager.instance.CurrentLevel + 1;
+
+            SceneManager.LoadScene("Nivel" + nextLevel); 
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      
+
+        player1 = (collision.tag == "2DPlayer1");
     }
 }
